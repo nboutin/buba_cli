e7211fe04597e87113df0e0b4b92c5d2fe27d4d2
Nicolas Boutin <boutwork@gmail.com>
2019-05-06 17:39:53 +0200

    add command project, import, list, help, quit

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,85 +1,174 @@
 
 #include <buba.h>
 
+#include <cstring>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace buba;
 using namespace std;
 
-int read_menu_input();
+using inputs_t = std::vector<std::string>;
+
+inputs_t read_menu_input();
 bool main_menu(Budget_Battle& buba);
-bool action_menu(Budget_Battle& buba);
+void command_project(Budget_Battle& buba, const inputs_t& inputs);
+void command_list(Budget_Battle& buba, const inputs_t& inputs);
+void command_import(Budget_Battle& buba, const inputs_t& inputs);
+void command_help();
 
 int main()
 {
-    cout << "\nBudget Battle CLI" << endl;
     Budget_Battle buba;
 
     while(main_menu(buba))
     {
-        while(action_menu(buba))
-        {
-        }
     }
 }
 
-int read_menu_input()
+inputs_t read_menu_input()
 {
+    cout << "buba_cli>" << flush;
+
     string input;
-    int input_value = 0;
+    inputs_t inputs;
+
     getline(std::cin, input);
+
     if(!input.empty())
     {
-        input_value = std::stoi(input);
+        const char* delim = " ";
+        char* token       = std::strtok(&input[0], delim);
+        while(token != nullptr)
+        {
+            inputs.push_back(token);
+            token = std::strtok(nullptr, delim);
+        }
     }
 
-    return input_value;
+    return inputs;
 }
 
 bool main_menu(Budget_Battle& buba)
 {
-    cout << "\nMain Menu:\n[1]Create Project\n[2]Open project\n[3]Exit" << endl;
+    const auto inputs  = read_menu_input();
+    const auto command = inputs.at(0);
 
-    switch(read_menu_input())
+    if(command == "project")
     {
-    case 1:
-        std::system("rm project_test");
-        buba.create_project("./project_test");
-        cout << "# project created" << endl;
-        break;
-    case 2:
-        buba.open_project("./project_test");
-        cout << "# project opened" << endl;
-        break;
-    case 3: return false;
+        command_project(buba, inputs);
     }
-
+    else if(command == "import")
+    {
+        command_import(buba, inputs);
+    }
+    else if(command == "list")
+    {
+        command_list(buba, inputs);
+    }
+    else if(command == "help")
+    {
+        command_help();
+    }
+    else if(command == "quit")
+    {
+        return false;
+    }
     return true;
 }
 
+void command_project(Budget_Battle& buba, const inputs_t& inputs)
+{
+    if(inputs.size() <= 1)
+    {
+        cerr << "error: missing arguments" << endl;
+        return;
+    }
+
+    const auto subcmd = inputs.at(1);
+    const auto param  = (inputs.size() >= 3) ? inputs.at(2) : "./test.bubap";
+
+    if(subcmd == "new")
+    {
+        //    	std::system("rm project_test");
+        if(!buba.create_project(param))
+            cerr << "error: cannot create project" << endl;
+        else
+            cout << "ok" << endl;
+    }
+    else if(subcmd == "open")
+    {
+        if(!buba.open_project(param))
+            cerr << "error: cannot open project" << endl;
+        else
+            cout << "ok" << endl;
+    }
+    else if(subcmd == "close")
+    {
+        cerr << "error: cannot close project" << endl;
+    }
+}
+
+void command_import(Budget_Battle& buba, const inputs_t& inputs)
+{
+    const auto param = (inputs.size() >= 2) ? inputs.at(2) : "test/res/test3.ofx";
+
+    if(!buba.import_ofx(param))
+        cerr << "error: cannot import ofx" << endl;
+    else
+        cout << "ok" << endl;
+}
+
+void command_list(Budget_Battle& buba, const inputs_t& inputs)
+{
+    (void) inputs;
+
+    auto transactions = buba.get_transactions_all();
+    for(auto o : transactions)
+    {
+        cout << "[" << o.fitid << "," << o.date << "," << o.description << "," << o.amount << "]"
+             << endl;
+    }
+}
+
+void command_help()
+{
+    cout << "Buba_cli" << endl;
+    cout << "project" << endl;
+    cout << "\tnew [pathname]" << endl;
+    cout << "\topen [pathname]" << endl;
+    cout << "\tclose" << endl;
+    cout << "import [pathname]" << endl;
+    cout << "list" << endl;
+    cout << "help" << endl;
+    cout << "quit" << endl;
+}
+
+#if 0
 bool action_menu(Budget_Battle& buba)
 {
-    cout << "\nAction Menu:\n[1]Import OFX\n[2]List transactions\n[3]Back" << endl;
+	cout << "\nAction Menu:\n[1]Import OFX\n[2]List transactions\n[3]Back" << endl;
 
-    switch(read_menu_input())
-    {
-    case 1:
-        buba.import_ofx("test/res/test3.ofx");
-        cout << "# OFX imported" << endl;
-        break;
-    case 2:
-    {
-        auto transactions = buba.get_transactions_all();
-        for(auto o : transactions)
-        {
-            cout << "[" << o.fitid << "," << o.date << "," << o.description << "," << o.amount
-                 << "]" << endl;
-        }
-        cout << "# Done" << endl;
-        break;
-    }
-    case 3: return false;
-    }
-    return true;
+	switch(read_menu_input())
+	{
+	case 1:
+		buba.import_ofx("test/res/test3.ofx");
+		cout << "# OFX imported" << endl;
+		break;
+	case 2:
+	{
+		auto transactions = buba.get_transactions_all();
+		for(auto o : transactions)
+		{
+			cout << "[" << o.fitid << "," << o.date << "," << o.description << "," << o.amount
+				 << "]" << endl;
+		}
+		cout << "# Done" << endl;
+		break;
+	}
+	case 3: return false;
+	}
+	return true;
 }
+#endif
