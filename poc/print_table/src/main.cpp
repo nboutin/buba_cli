210afd4dd3632e4ebb5b3469a67d5e0c19ee72c8
Nicolas Boutin <boutwork@gmail.com>
2019-05-10 11:47:51 +0200

    simple print

--- poc/print_table/src/main.cpp
+++ poc/print_table/src/main.cpp
@@ -1,5 +1,43 @@
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+
++--Title1--+--Title2--+
+|value     |value     +
+
+
+*/
+
+using line_t = std::vector<std::string>;
+
+void print_table(const std::vector<line_t> table)
+{
+    using namespace std;
+
+    //    const auto sep_title = '+';
+    const auto sep_value = '|';
+
+    for(auto line : table)
+    {
+        for(auto element : line)
+        {
+            cout << sep_value << element;
+        }
+        cout << sep_value << "\n";
+    }
+    cout << flush;
+}
+
 int main()
 {
-	return 0;
+    using namespace std;
+
+    vector<line_t> table{{"Title1", "Title2"}, {"value11", "value12"}, {"value21", "value22"}};
+
+    print_table(table);
+
+    return 0;
 }
